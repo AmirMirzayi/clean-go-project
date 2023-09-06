aac71446aa9364bf017a9a7ae5657b6c3de290b4
Amir <mirzayi994@gmail.com>
2023-09-06 17:06:53 +0330

    load config from env

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,22 @@
 package config
 
-type HttpServer struct{
-	Address string
-	Port int
+import (
+	"github.com/caarlos0/env/v9"
+)
+
+type HttpServer struct {
+	Address string `env:"HTTP_SERVER_ADDRESS"`
+	Port    int    `env:"HTTP_SERVER_PORT"`
 }
 
-type Config struct{
+type Config struct {
 	HttpServer
+}
+
+func LoadConfig() Config {
+	cfg := Config{}
+	if err := env.Parse(&cfg); err != nil {
+		panic(err)
+	}
+	return cfg
 }
