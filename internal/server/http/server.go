ed2487bfb22f973f6d3b7ef29c4f05d5968d35ce
Amir <mirzayi994@gmail.com>
2023-09-06 17:08:25 +0330

    init http server

--- internal/server/http/server.go
+++ internal/server/http/server.go
@@ -1,2 +1,33 @@
 package http
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/AmirMirzayi/clean-go-project/internal/config"
+)
+
+type HttpServer struct {
+	server http.Server
+	mux    *http.ServeMux
+}
+
+func NewServer(cfg config.HttpServer) *HttpServer {
+	return &HttpServer{
+		server: http.Server{
+			Addr: fmt.Sprintf("%s:%d", cfg.Address, cfg.Port),
+		},
+		mux: http.NewServeMux(),
+	}
+}
+
+func (s *HttpServer) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	s.mux.HandleFunc(pattern, handler)
+}
+
+func (s *HttpServer) Start() {
+	log.Println("http server started on address: ", s.server.Addr)
+	s.server.Handler = s.mux
+	s.server.ListenAndServe()
+}
