71dbfa82219c9cb0b64f479006083269297917c7
Amir <mirzayi994@gmail.com>
2023-09-06 17:07:30 +0330

    init http by config

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,1 +1,14 @@
-package server
+package main
+
+import (
+	"github.com/AmirMirzayi/clean-go-project/internal/config"
+	"github.com/AmirMirzayi/clean-go-project/internal/server/http"
+	"github.com/AmirMirzayi/clean-go-project/pkg/api"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+	server := http.NewServer(cfg.HttpServer)
+	api.InitRouting(server)
+	server.Start()
+}
